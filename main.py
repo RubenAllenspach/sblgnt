d3e75554ef137fc751959f386b0560e66c67aa78
RubenAllenspach <14939197+RubenAllenspach@users.noreply.github.com>
2019-05-03 16:20:21 +0200

    updated table structure, put txt files in Folder

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 import time
 import pymysql
-from os import listdir, walk
+from os import listdir
 from os.path import isfile, join, dirname
 
-mypath = join(dirname(__file__), 'books/')
+my_path = join(dirname(__file__), 'books/')
 
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+only_files = [f for f in listdir(my_path) if isfile(join(my_path, f))]
 
 db = pymysql.connect("localhost", "root", "pswd1234", "allensp6_greek")
 cursor = db.cursor()
 
 time_start = time.time()
 
-for file in onlyfiles:
-    with open(mypath + file, encoding="utf8") as f:
+for file in only_files:
+    with open(my_path + file, encoding="utf8") as f:
         for line in f:
             path, part_of_speech, parsing_code, text, word, normalized_word, lemma = line.strip().split()
-            print(path, part_of_speech, parsing_code, text, word, normalized_word, lemma)
+            print(path[0:2], path[2:4], path[4:6], part_of_speech, parsing_code, text, word, normalized_word, lemma)
 
             try:
                 cursor.execute(
                     "INSERT INTO lwt_greek_morphgnt (\
-                        path,\
+                        book,\
+                        chapter,\
+                        verse,\
                         part_of_speech,\
                         parsing_code,\
                         text,\
@@ -35,9 +37,11 @@ for file in onlyfiles:
                         '%s',\
                         '%s',\
                         '%s',\
+                        '%s',\
+                        '%s',\
                         '%s'\
-                    )" % \
-                    (path, part_of_speech, parsing_code, text, word, normalized_word, lemma)
+                    )" %
+                    (path[0:2], path[2:4], path[4:6], part_of_speech, parsing_code, text, word, normalized_word, lemma)
                 )
                 # commit changes to db
                 db.commit()
@@ -49,68 +53,3 @@ db.close()
 
 print('starting time: ' + str(time_start))
 print('ending time: ' + str(time.time()))
-
-
-
-# import pymysql
-# import pprint
-# import os.path
-
-# Open database connection
-# db = pymysql.connect("localhost", "root", "pswd1234", "allensp6_greek")
-
-# prepare a cursor object using cursor() method
-# cursor = db.cursor()
-
-# i = 0
-
-# with open(os.path.join(os.path.dirname(__file__), "gnt_data/tokens.txt"), encoding="utf8") as f:
-    # for line in f:
-        # i += 1
-
-        # token_id, token_text, token_text_normalized, part_of_speech, morphological_tag_old, morphological_tag_new, lemma = line.strip().split()
-
-        # print(token_id, token_text, token_text_normalized, part_of_speech, morphological_tag_old, morphological_tag_new, lemma)
-
-        # if i > 10:
-        #     break
-
-        # try:
-        #     cursor.execute(
-        #         "INSERT INTO lwt_greek_tokens (\
-        #             token_id,\
-        #             token_text,\
-        #             token_text_normalized,\
-        #             part_of_speech,\
-        #             morphological_tag_old,\
-        #             morphological_tag_new,\
-        #             lemma\
-        #         ) VALUES (\
-        #             '%d',\
-        #             '%s',\
-        #             '%s',\
-        #             '%s',\
-        #             '%s',\
-        #             '%s',\
-        #             '%s'\
-        #         )" % \
-        #         (int(token_id), token_text, token_text_normalized, part_of_speech, morphological_tag_old, morphological_tag_new, lemma)
-        #     )
-            # commit changes to db
-            # db.commit()
-        # except:
-            # Rollback in case there is any error
-            # db.rollback()
-
-# Fetch a single row using fetchone() method.
-# data = cursor.fetchall()
-
-# ugly print
-# print(data)
-
-# pretty print
-# pp = pprint.PrettyPrinter(indent=4)
-# pp.pprint(data)
-
-# disconnect from server
-# db.close()
